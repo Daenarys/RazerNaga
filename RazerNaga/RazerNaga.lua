be9a9b61eeb223fb022bdcedfbe8d6e80ec224b9
Daenarys <androidwoz@gmail.com>
2023-07-13 11:15:59 +0200

    frames -> minimap

--- RazerNaga/RazerNaga.lua
+++ RazerNaga/RazerNaga.lua
@@ -1060,7 +1060,7 @@ function RazerNaga:IsLinkedOpacityEnabled()
 end
 
 --load and position the lfg eye
-if not (IsAddOnLoaded("ClassicFrames")) then
+if not (IsAddOnLoaded("ClassicMinimap")) then
 	hooksecurefunc(QueueStatusButton, "UpdatePosition", function(self)
 		self:SetParent(MinimapBackdrop)
 		self:SetFrameLevel(6)
