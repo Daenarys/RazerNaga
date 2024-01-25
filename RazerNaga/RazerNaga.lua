71151933d2cc88a523d889a1f7d5290d237c30ef
Daenarys <androidwoz@gmail.com>
2024-01-26 00:25:40 +0100

    use df bag/menu bar art

--- RazerNaga/RazerNaga.lua
+++ RazerNaga/RazerNaga.lua
@@ -1056,15 +1056,15 @@ if not (IsAddOnLoaded("ClassicFrames")) then
 		self:SetScale(0.85)
 	end)
 
-	--queuestatusframe
-	hooksecurefunc(QueueStatusFrame, "UpdatePosition", function(self)
-		self:ClearAllPoints();
-		self:SetPoint("TOPRIGHT", QueueStatusButton, "TOPLEFT", 0, 0);
-	end)
-
+	--adjust the queuestatus position
 	hooksecurefunc("QueueStatusDropDown_Show", function()
 		DropDownList1:ClearAllPoints()
-		DropDownList1:SetPoint("TOPLEFT", QueueStatusButton, "BOTTOMLEFT")
+		DropDownList1:SetPoint("BOTTOMLEFT", QueueStatusButton, "BOTTOMLEFT", 0, -62)
+	end)
+
+	hooksecurefunc(QueueStatusFrame, "UpdatePosition", function(self)
+		self:ClearAllPoints();
+		self:SetPoint("TOPRIGHT", QueueStatusButton, "TOPLEFT", -1, 1);
 	end)
 end
 
