2967f81c6e9848d045fd3c748714498fb2c6fc4f
Daenarys <androidwoz@gmail.com>
2024-01-27 15:20:42 +0100

    restore 5.4.8 castbar

--- RazerNaga_Cast/castBar.lua
+++ RazerNaga_Cast/castBar.lua
@@ -28,6 +28,8 @@ function CastBar:New()
 		f.cast = CastingBar:New(f)
 		f.header:SetParent(nil)
 		f.header:ClearAllPoints()
+		f:SetWidth(240) 
+		f:SetHeight(24)
 	end
 
 	f:UpdateText()
@@ -52,9 +54,9 @@ end
 
 function CastBar:UpdateText()
 	if self.sets.showText then
-		self.cast.Time:Show()
+		self.cast.time:Show()
 	else
-		self.cast.Time:Hide()
+		self.cast.time:Hide()
 	end
 	self.cast:AdjustWidth()
 end
@@ -92,27 +94,49 @@ function CastingBar:New(parent)
 	local f = self:Bind(CreateFrame('StatusBar', 'RazerNagaCastingBar', parent, 'RazerNagaCastingBarTemplate'))
 	f:SetPoint('CENTER')
 
+	local name = f:GetName()
+	local _G = getfenv(0)
+	f.time = _G[name .. 'Time']
+	f.text = _G[name .. 'Text']
+	f.borderTexture = _G[name .. 'Border']
+	f.flashTexture = _G[name .. 'Flash']
+
 	f.normalWidth = f:GetWidth()
-	f:SetScript('OnUpdate', self.OnUpdate)
+	f:SetScript('OnUpdate', f.OnUpdate)
+	f:SetScript('OnEvent', f.OnEvent)
 
 	return f
 end
 
+function CastingBar:OnEvent(event, ...)
+	CastingBarFrame_OnEvent(self, event, ...)
+
+	local unit, spell = ...
+	if unit == self.unit then
+		if event == 'UNIT_SPELLCAST_FAILED' or event == 'UNIT_SPELLCAST_INTERRUPTED' then
+			self.failed = true
+		elseif event == 'UNIT_SPELLCAST_START' or event == 'UNIT_SPELLCAST_CHANNEL_START' then
+			self.failed = nil
+		end
+		self:UpdateColor(spell)
+	end
+end
+
 function CastingBar:OnUpdate(elapsed)
-	CastingBarMixin.OnUpdate(self, elapsed)
+	CastingBarFrame_OnUpdate(self, elapsed)
 
 	if self.casting then
-		self.Time:SetFormattedText('%.1f', self.maxValue - self.value)
+		self.time:SetFormattedText('%.1f', self.maxValue - self.value)
 		self:AdjustWidth()
 	elseif self.channeling then
-		self.Time:SetFormattedText('%.1f', self.value)
+		self.time:SetFormattedText('%.1f', self.value)
 		self:AdjustWidth()
 	end
 end
 
 function CastingBar:AdjustWidth()
-	local textWidth = self.Text:GetStringWidth() + TEXT_PADDING
-	local timeWidth = (self.Time:IsShown() and (self.Time:GetStringWidth() + 4) * 2) or 0
+	local textWidth = self.text:GetStringWidth() + TEXT_PADDING
+	local timeWidth = (self.time:IsShown() and (self.time:GetStringWidth() + 4) * 2) or 0
 	local width = textWidth + timeWidth
 
 	if width < self.normalWidth then
@@ -123,10 +147,22 @@ function CastingBar:AdjustWidth()
 	
 	if diff > TEXT_PADDING then -- is the difference big enough to redraw the bar ?
 		self:SetWidth(width)
-		self.Border:SetWidth(width * BORDER_SCALE)
-		self.Flash:SetWidth(width * BORDER_SCALE)
+		self.borderTexture:SetWidth(width * BORDER_SCALE)
+		self.flashTexture:SetWidth(width * BORDER_SCALE)
 
 		self:GetParent():Layout()
+	end
+end
+
+function CastingBar:UpdateColor(spell)
+	if self.failed then
+		self:SetStatusBarColor(0.86, 0.08, 0.24)
+	elseif spell and IsHelpfulSpell(spell) then
+		self:SetStatusBarColor(0.31, 0.78, 0.47)
+	elseif spell and IsHarmfulSpell(spell) then
+		self:SetStatusBarColor(0.63, 0.36, 0.94)
+	else
+		self:SetStatusBarColor(1, 0.7, 0)
 	end
 end
 
