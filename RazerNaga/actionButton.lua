a6d3b1371624d49c65f18fe27e1865d797843ad4
Daenarys <androidwoz@gmail.com>
2024-02-04 12:58:51 +0100

    disable crap effects

--- RazerNaga/actionButton.lua
+++ RazerNaga/actionButton.lua
@@ -192,3 +192,18 @@ function ActionButton:Skin()
 		end
     end
 end
+
+if (ActionBarActionEventsFrame) then
+    ActionBarActionEventsFrame:UnregisterEvent("UNIT_SPELLCAST_INTERRUPTED")
+    ActionBarActionEventsFrame:UnregisterEvent("UNIT_SPELLCAST_SUCCEEDED")
+    ActionBarActionEventsFrame:UnregisterEvent("UNIT_SPELLCAST_START")
+    ActionBarActionEventsFrame:UnregisterEvent("UNIT_SPELLCAST_STOP")
+    ActionBarActionEventsFrame:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_START")
+    ActionBarActionEventsFrame:UnregisterEvent("UNIT_SPELLCAST_CHANNEL_STOP")
+    ActionBarActionEventsFrame:UnregisterEvent("UNIT_SPELLCAST_RETICLE_TARGET")
+    ActionBarActionEventsFrame:UnregisterEvent("UNIT_SPELLCAST_RETICLE_CLEAR")
+    ActionBarActionEventsFrame:UnregisterEvent("UNIT_SPELLCAST_EMPOWER_START")
+    ActionBarActionEventsFrame:UnregisterEvent("UNIT_SPELLCAST_EMPOWER_STOP")
+    ActionBarActionEventsFrame:UnregisterEvent("UNIT_SPELLCAST_SENT")
+    ActionBarActionEventsFrame:UnregisterEvent("UNIT_SPELLCAST_FAILED")
+end
