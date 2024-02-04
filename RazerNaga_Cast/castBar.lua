c2d313aeab32eaf991635dbcce7b3329fc50aaed
Daenarys <androidwoz@gmail.com>
2024-02-04 14:43:18 +0100

    revert to v3.2.1

--- RazerNaga_Cast/castBar.lua
+++ RazerNaga_Cast/castBar.lua
@@ -92,13 +92,27 @@ function CastingBar:New(parent)
 	f:SetPoint('CENTER')
 
 	f.normalWidth = f:GetWidth()
-	f:SetScript('OnUpdate', self.OnUpdate)
+	f:SetScript('OnUpdate', f.OnUpdate)
+	f:SetScript('OnEvent', f.OnEvent)
 
 	return f
 end
 
+function CastingBar:OnEvent(event, ...)
+	CastingBarFrame_OnEvent(self, event, ...)
+
+	local unit = self.unit
+	local spell = UnitCastingInfo(unit)
+	if event == 'UNIT_SPELLCAST_FAILED' or event == 'UNIT_SPELLCAST_INTERRUPTED' then
+		self.failed = true
+	elseif event == 'UNIT_SPELLCAST_START' or event == 'UNIT_SPELLCAST_CHANNEL_START' then
+		self.failed = nil
+	end
+	self:UpdateColor(spell)
+end
+
 function CastingBar:OnUpdate(elapsed)
-	CastingBarMixin.OnUpdate(self, elapsed)
+	CastingBarFrame_OnUpdate(self, elapsed)
 
 	if self.casting then
 		self.Time:SetFormattedText('%.1f', self.maxValue - self.value)
@@ -117,15 +131,25 @@ function CastingBar:AdjustWidth()
 	if width < self.normalWidth then
 		width = self.normalWidth
 	end
-	 	
+
 	local diff = math.abs(width - self:GetWidth())
-	
+
 	if diff > TEXT_PADDING then
 		self:SetWidth(width)
 		self.Border:SetWidth(width * BORDER_SCALE)
 		self.Flash:SetWidth(width * BORDER_SCALE)
 
 		self:GetParent():Layout()
+	end
+end
+
+function CastingBar:UpdateColor(spell)
+	if self.failed then
+		self:SetStatusBarColor(0.86, 0.08, 0.24)
+	elseif spell and IsHelpfulSpell(spell) then
+		self:SetStatusBarColor(0.31, 0.78, 0.47)
+	elseif spell and IsHarmfulSpell(spell) then
+		self:SetStatusBarColor(0.63, 0.36, 0.94)
 	end
 end
 
