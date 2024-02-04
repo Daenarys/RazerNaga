e26d49bc470f8676552d6a7de4b935c1587abc87
Daenarys <androidwoz@gmail.com>
2024-02-04 14:02:13 +0100

    cleanup

--- RazerNaga/actionButton.lua
+++ RazerNaga/actionButton.lua
@@ -170,13 +170,6 @@ function ActionButton:UpdateMacro()
 	end
 end
 
-function ActionButton:SetFlyoutDirection(direction)
-	if InCombatLockdown() then return end
-	
-	self:SetAttribute('flyoutDirection', direction)
-	self:UpdateFlyout()
-end
-
 --utility function, resyncs the button's current action, modified by state
 function ActionButton:LoadAction()
 	local state = self:GetParent():GetAttribute('state-page')
