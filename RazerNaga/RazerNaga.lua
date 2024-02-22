b82e8cf6fc94fc172e8b55a22a8259f75405de89
Daenarys <androidwoz@gmail.com>
2024-02-22 09:20:17 +0100

    update hider

--- RazerNaga/RazerNaga.lua
+++ RazerNaga/RazerNaga.lua
@@ -204,76 +204,70 @@ function RazerNaga:HideBlizzard()
 	HiddenFrame:SetAllPoints(UIParent)
 	HiddenFrame:Hide()
 
-	local function purgeKey(t, k)
-		t[k] = nil
-		local c = 42
-		repeat
-			if t[c] == nil then
-				t[c] = nil
-			end
-			c = c + 1
-		until issecurevariable(t, k)
+	local function apply(func, arg, ...)
+		if select('#', ...) > 0 then
+			return func(arg), apply(func, ...)
+		end
+
+		return func(arg)
 	end
 
-	local function hideActionBarFrame(frame, clearEvents)
-		if frame then
-			if clearEvents then
-				frame:UnregisterAllEvents()
-			end
+	local function hide(frame)
+		if not frame then
+			return
+		end
 
-			if frame.system then
-				purgeKey(frame, "isShownExternal")
-			end
+		if frame.HideBase then
+			frame:HideBase()
+		else
+			frame:Hide()
+		end
+		
+		frame:SetParent(HiddenFrame)
+	end
 
-			if frame.HideBase then
-				frame:HideBase()
-			else
-				frame:Hide()
-			end
+	apply(hide,
+		MainMenuBar,
+		MultiBarBottomLeft,
+		MultiBarBottomRight,
+		MultiBarLeft,
+		MultiBarRight,
+		MultiBar5,
+		MultiBar6,
+		MultiBar7,
+		MicroButtonAndBagsBar,
+		StanceBar,
+		PossessActionBar,
+		MultiCastActionBarFrame,
+		PetActionBar,
+		StatusTrackingBarManager,
+		MainMenuBarVehicleLeaveButton,
+		BagsBar,
+		MicroMenu,
+		MicroMenuContainer
+	)
 
-			frame:SetParent(HiddenFrame)
+	local function disableActionButton(name)
+		local button = _G[name]
+		if button then
+			button:UnregisterAllEvents()
+			button:SetAttribute('statehidden', true)
+			button:Hide()
+		else
+			self:Printf('Action Button %q could not be found', name)
 		end
 	end
 
-	local function hideActionButton(button)
-		if not button then return end
-
-		button:Hide()
-		button:UnregisterAllEvents()
-		button:SetAttribute("statehidden", true)
+	for id = 1, NUM_ACTIONBAR_BUTTONS do
+		disableActionButton(('ActionButton%d'):format(id))
+		disableActionButton(('MultiBarRightButton%d'):format(id))
+		disableActionButton(('MultiBarLeftButton%d'):format(id))
+		disableActionButton(('MultiBarBottomRightButton%d'):format(id))
+		disableActionButton(('MultiBarBottomLeftButton%d'):format(id))
+		disableActionButton(('MultiBar5Button%d'):format(id))
+		disableActionButton(('MultiBar6Button%d'):format(id))
+		disableActionButton(('MultiBar7Button%d'):format(id))
 	end
-
-	hideActionBarFrame(MainMenuBar, true)
-	hideActionBarFrame(MultiBarBottomLeft, true)
-	hideActionBarFrame(MultiBarBottomRight, true)
-	hideActionBarFrame(MultiBarLeft, true)
-	hideActionBarFrame(MultiBarRight, true)
-	hideActionBarFrame(MultiBar5, true)
-	hideActionBarFrame(MultiBar6, true)
-	hideActionBarFrame(MultiBar7, true)
-
-	-- Hide MultiBar Buttons, but keep the bars alive
-	for i=1,12 do
-		hideActionButton(_G["ActionButton"..i])
-		hideActionButton(_G["MultiBarBottomLeftButton"..i])
-		hideActionButton(_G["MultiBarBottomRightButton"..i])
-		hideActionButton(_G["MultiBarRightButton"..i])
-		hideActionButton(_G["MultiBarLeftButton"..i])
-		hideActionButton(_G["MultiBar5Button"..i])
-		hideActionButton(_G["MultiBar6Button"..i])
-		hideActionButton(_G["MultiBar7Button"..i])
-	end
-
-	hideActionBarFrame(MicroButtonAndBagsBar, false)
-	hideActionBarFrame(StanceBar, true)
-	hideActionBarFrame(PossessActionBar, true)
-	hideActionBarFrame(MultiCastActionBarFrame, false)
-	hideActionBarFrame(PetActionBar, false)
-	hideActionBarFrame(StatusTrackingBarManager, false)
-	hideActionBarFrame(MainMenuBarVehicleLeaveButton, true)
-	hideActionBarFrame(BagsBar, true)
-	hideActionBarFrame(MicroMenu, true)
-	hideActionBarFrame(MicroMenuContainer, true)
 end
 
 function RazerNaga:SetUseOverrideUI(enable)
