d1436ccfd1c7d9020b8290f0a2edf36452980cf8
Daenarys <androidwoz@gmail.com>
2024-02-25 22:11:24 +0100

    drop this for now

--- RazerNaga_Skin/skins/SettingsPanel.lua
+++
@@ -1,8 +0,0 @@
-if not _G.SettingsPanel then return end
-
-SettingsPanel.CloseButton:Hide()
-
-_G.SettingsPanel:HookScript("OnShow", function(self)
-	self:ClearAllPoints()
-	self:SetPoint("CENTER", 0, 5)
-end)
