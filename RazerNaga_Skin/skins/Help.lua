7fb0b9c7011e8994d30485273e03c20a1c372e5c
Daenarys <androidwoz@gmail.com>
2024-02-29 17:24:36 +0100

    cleanup

--- RazerNaga_Skin/skins/Help.lua
+++
@@ -1,6 +0,0 @@
-if not _G.HelpFrame then return end
-
-_G.HelpFrame:HookScript("OnShow", function(self)
-	self:ClearAllPoints()
-	self:SetPoint("CENTER", 0, 24)
-end)
--- RazerNaga_Skin/skins/TotemFrame.lua
+++
@@ -1,6 +0,0 @@
-if not _G.TotemFrame then return end
-
-hooksecurefunc(TotemFrame, "Update", function(self)
-	self:ClearAllPoints()
-	self:SetPoint("TOPLEFT", PlayerFrame, "TOPLEFT", -5, -90)
-end)
