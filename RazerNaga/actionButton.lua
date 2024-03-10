1959c08113cf5ad6ea1ebf1e04400236b7fd1b6a
Daenarys <androidwoz@gmail.com>
2024-03-10 17:35:59 +0100

    changed count drawlayer

--- RazerNaga/actionButton.lua
+++ RazerNaga/actionButton.lua
@@ -73,6 +73,7 @@ local function skinActionButton(self)
     self.Flash:SetAllPoints()
     self.Count:ClearAllPoints()
     self.Count:SetPoint("BOTTOMRIGHT", -2, 2)
+    self.Count:SetDrawLayer("ARTWORK", 2)
     self.FlyoutBorderShadow:SetSize(48, 48)
     if self.IconMask then
         self.IconMask:Hide()
