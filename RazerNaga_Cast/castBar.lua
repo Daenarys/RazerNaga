4ea348c6ce5d855151e209697c0d865ca9abfa61
Daenarys <androidwoz@gmail.com>
2024-03-29 19:41:48 +0100

    version with 10.x skin

--- RazerNaga_Cast/castBar.lua
+++ RazerNaga_Cast/castBar.lua
@@ -30,7 +30,6 @@ function CastBar:New()
 		f.cast = CastingBar:New(f)
 	end
 
-	f:UpdateText()
 	f:Layout()
 
 	return f
@@ -40,32 +39,13 @@ function CastBar:GetDefaults()
 	return {
 		point = 'CENTER',
 		x = 0,
-		y = 30,
-		showText = true
+		y = 30
 	}
-end
-
-function CastBar:ToggleText(enable)
-	self.sets.showText = enable or false
-	self:UpdateText()
-end
-
-function CastBar:UpdateText()
-	if self.sets.showText then
-		self.cast.time:Show()
-	else
-		self.cast.time:Hide()
-	end
-	self.cast:AdjustWidth()
 end
 
 function CastBar:CreateMenu()
 	local menu = RazerNaga:NewMenu(self.id)
 	local panel = menu:NewPanel(LibStub('AceLocale-3.0'):GetLocale('RazerNaga-Config').Layout)
-
-	local time = panel:NewCheckButton(RazerNaga_SHOW_TIME)
-	time:SetScript('OnClick', function(b) self:ToggleText(b:GetChecked()) end)
-	time:SetScript('OnShow', function(b) b:SetChecked(self.sets.showText) end)
 
 	panel:NewOpacitySlider()
 	panel:NewFadeSlider()
@@ -86,56 +66,22 @@ end
 
 CastingBar = RazerNaga:CreateClass('StatusBar')
 
-local BORDER_SCALE = 197/150
-local TEXT_PADDING = 18
-
 function CastingBar:New(parent)
 	local f = self:Bind(CreateFrame('StatusBar', 'RazerNagaCastingBar', parent, 'RazerNagaCastingBarTemplate'))
-	f:SetPoint('CENTER')
-
-	local name = f:GetName()
-	local _G = getfenv(0)
-	f.time = _G[name .. 'Time']
-	f.text = _G[name .. 'Text']
-	f.borderTexture = _G[name .. 'Border']
-	f.flashTexture = _G[name .. 'Flash']
-
-	f.normalWidth = f:GetWidth()
-	f:SetScript('OnUpdate', f.OnUpdate)
+	f:SetPoint('CENTER', 0, 5)
 
 	return f
 end
 
-function CastingBar:OnUpdate(elapsed)
-	CastingBarFrame_OnUpdate(self, elapsed)
+RazerNagaCastingBarMixin = {}
 
-	if self.casting then
-		self.time:SetFormattedText('%.1f', self.maxValue - self.value)
-		self:AdjustWidth()
-	elseif self.channeling then
-		self.time:SetFormattedText('%.1f', self.value)
-		self:AdjustWidth()
-	end
-end
-
-function CastingBar:AdjustWidth()
-	local textWidth = self.text:GetStringWidth() + TEXT_PADDING
-	local timeWidth = (self.time:IsShown() and (self.time:GetStringWidth() + 4) * 2) or 0
-	local width = textWidth + timeWidth
-
-	if width < self.normalWidth then
-		width = self.normalWidth
-	end
-
-	local diff = math.abs(width - self:GetWidth())
-
-	if diff > TEXT_PADDING then
-		self:SetWidth(width)
-		self.borderTexture:SetWidth(width * BORDER_SCALE)
-		self.flashTexture:SetWidth(width * BORDER_SCALE)
-
-		self:GetParent():Layout()
-	end
+function RazerNagaCastingBarMixin:OnLoad()
+	local showTradeSkills = true
+	local showShieldNo = false
+	CastingBarMixin.OnLoad(self, "player", showTradeSkills, showShieldNo)
+	self.Icon:Hide()
+	self.Text:ClearAllPoints()
+	self.Text:SetPoint("TOP", 0, -9)
 end
 
 --hide the old casting bar
