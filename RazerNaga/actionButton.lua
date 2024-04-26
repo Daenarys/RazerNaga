eb03821a3555f3a7c0733d7abadaa39e923263b4
Daenarys <androidwoz@gmail.com>
2024-04-26 16:51:15 +0200

    use some 10.x art

--- RazerNaga/actionButton.lua
+++ RazerNaga/actionButton.lua
@@ -132,17 +132,6 @@ function ActionButtonMixin:OnCreate(id)
 
     -- enable cooldown bling
     self.cooldown:SetDrawBling(true)
-
-    hooksecurefunc(self, "Update", function()
-        local texture = GetActionTexture(self.action)
-
-        if not ( texture ) then
-            local hotkey = self.HotKey
-            if not ( hotkey:GetText() == RANGE_INDICATOR ) then
-                hotkey:SetVertexColor(0.6, 0.6, 0.6)
-            end
-        end
-    end)
 end
 
 function ActionButtonMixin:UpdateOverrideBindings()
@@ -538,24 +527,6 @@ end)
 hooksecurefunc("CooldownFrame_Set", function(self)
     if not self:IsForbidden() then
         self:SetEdgeTexture("Interface\\Cooldown\\edge")
-    end
-end)
-
-hooksecurefunc("ActionButton_UpdateRangeIndicator", function(self, checksRange, inRange)
-    if ( self.HotKey:GetText() == RANGE_INDICATOR ) then
-        if ( checksRange ) then
-            if ( inRange ) then
-                self.HotKey:SetVertexColor(LIGHTGRAY_FONT_COLOR:GetRGB())
-            else
-                self.HotKey:SetVertexColor(RED_FONT_COLOR:GetRGB())
-            end
-        end
-    else
-        if ( checksRange and not inRange ) then
-            self.HotKey:SetVertexColor(RED_FONT_COLOR:GetRGB())
-        else
-            self.HotKey:SetVertexColor(LIGHTGRAY_FONT_COLOR:GetRGB())
-        end
     end
 end)
 
---
+++ RazerNaga_Skin/skins/WorldMap.lua
@@ -0,0 +1,9 @@
+if not _G.WorldMapFrame then return end
+
+for _, f in next, WorldMapFrame.overlayFrames do
+	if WorldMapActivityTrackerMixin and f.OnLoad == WorldMapActivityTrackerMixin.OnLoad then
+		hooksecurefunc(f, "Show", function()
+			f:Hide()
+		end)
+	end
+end
