bfb40c77673b62bea19bc4e4c8b8b2c403b35cea
Daenarys <androidwoz@gmail.com>
2024-05-04 11:28:08 +0200

    cleanup

--- RazerNaga_Skin/skins/WorldMap.lua
+++
@@ -1,9 +0,0 @@
-if not _G.WorldMapFrame then return end
-
-for _, f in next, WorldMapFrame.overlayFrames do
-	if WorldMapActivityTrackerMixin and f.OnLoad == WorldMapActivityTrackerMixin.OnLoad then
-		hooksecurefunc(f, "Show", function()
-			f:Hide()
-		end)
-	end
-end
