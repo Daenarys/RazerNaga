3562a622ffa22e1600339a2ead8ade9de30c121c
Daenarys <androidwoz@gmail.com>
2024-05-10 13:27:31 +0200

    cleanup

--- RazerNaga_Skin/skins/ObjectiveTracker.lua
+++
@@ -1,8 +0,0 @@
-if not _G.ObjectiveTrackerFrame then return end
-
-hooksecurefunc("ObjectiveTracker_Update", function()
-	ObjectiveTrackerFrame.HeaderMenu.MinimizeButton:SetDisabledAtlas("RedButton-MiniCondense-disabled")
-	ObjectiveTrackerFrame.HeaderMenu.MinimizeButton:SetNormalAtlas("RedButton-MiniCondense")
-	ObjectiveTrackerFrame.HeaderMenu.MinimizeButton:SetPushedAtlas("RedButton-MiniCondense-pressed")
-	ObjectiveTrackerFrame.HeaderMenu.MinimizeButton:SetHighlightAtlas("RedButton-Highlight")
-end)
