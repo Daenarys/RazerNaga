d64f7eb1238276f7f674cd43f7a8110a2b8539b4
Daenarys <androidwoz@gmail.com>
2024-05-14 11:33:40 +0200

    cleanup

--- RazerNaga/RazerNaga.lua
+++ RazerNaga/RazerNaga.lua
@@ -883,24 +883,27 @@ function RazerNaga:SetFirstLoad(enable)
 end
 
 --queuestatusbutton
-hooksecurefunc(QueueStatusButton, "UpdatePosition", function(self)
-	self:SetParent(MinimapBackdrop)
-	self:SetFrameLevel(6)
-	self:ClearAllPoints()
-	self:SetPoint("TOPLEFT", MinimapBackdrop, "TOPLEFT", 45, -217)
-	self:SetScale(0.85)
-end)
+if not (C_AddOns.IsAddOnLoaded("ClassicFrames")) then
+	--load and position the lfg eye
+	hooksecurefunc(QueueStatusButton, "UpdatePosition", function(self)
+		self:SetParent(MinimapBackdrop)
+		self:SetFrameLevel(6)
+		self:ClearAllPoints()
+		self:SetPoint("TOPLEFT", MinimapBackdrop, "TOPLEFT", 45, -217)
+		self:SetScale(0.85)
+	end)
 
---queuestatusframe
-hooksecurefunc(QueueStatusFrame, "UpdatePosition", function(self)
-	self:ClearAllPoints()
-	self:SetPoint("TOPRIGHT", QueueStatusButton, "TOPLEFT")
-end)
+	--queuestatusframe
+	hooksecurefunc(QueueStatusFrame, "UpdatePosition", function(self)
+		self:ClearAllPoints()
+		self:SetPoint("TOPRIGHT", QueueStatusButton, "TOPLEFT")
+	end)
 
-hooksecurefunc("QueueStatusDropDown_Show", function()
-	DropDownList1:ClearAllPoints()
-	DropDownList1:SetPoint("TOPLEFT", QueueStatusButton, "BOTTOMLEFT")
-end)
+	hooksecurefunc("QueueStatusDropDown_Show", function()
+		DropDownList1:ClearAllPoints()
+		DropDownList1:SetPoint("TOPLEFT", QueueStatusButton, "BOTTOMLEFT")
+	end)
+end
 
 --[[ Incompatibility Check ]]--
 
