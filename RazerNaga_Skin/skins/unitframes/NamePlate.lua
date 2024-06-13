c44a4fcac92ce76c9a78a374e96181fd8fc4607b
Daenarys <androidwoz@gmail.com>
2024-06-13 14:55:01 +0200

    cleanup

--- RazerNaga_Skin/skins/unitframes/NamePlate.lua
+++
@@ -1,8 +0,0 @@
-hooksecurefunc("CompactUnitFrame_UpdateClassificationIndicator", function(frame)
-	if frame.classificationIndicator then
-		local classification = UnitClassification(frame.unit)
-		if (classification == "rare") then
-			frame.classificationIndicator:SetAtlas("nameplates-icon-elite-silver")
-		end
-	end
-end)
