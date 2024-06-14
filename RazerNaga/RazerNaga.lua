5136d1e3096b39912ae3bab99219a1f6fcb4bb23
Daenarys <androidwoz@gmail.com>
2024-06-14 16:52:13 +0200

    cleanup

--- RazerNaga/RazerNaga.lua
+++ RazerNaga/RazerNaga.lua
@@ -909,19 +909,22 @@ function RazerNaga:SetFirstLoad(enable)
 end
 
 --queuestatusbutton
-hooksecurefunc(QueueStatusButton, "UpdatePosition", function(self)
-	self:SetParent(MinimapBackdrop)
-	self:SetFrameLevel(6)
-	self:ClearAllPoints()
-	self:SetPoint("TOPLEFT", MinimapBackdrop, "TOPLEFT", 45, -217)
-	self:SetScale(0.85)
-end)
+if not (C_AddOns.IsAddOnLoaded("ClassicFrames")) then
+	--load and position the lfg eye
+	hooksecurefunc(QueueStatusButton, "UpdatePosition", function(self)
+		self:SetParent(MinimapBackdrop)
+		self:SetFrameLevel(6)
+		self:ClearAllPoints()
+		self:SetPoint("TOPLEFT", MinimapBackdrop, "TOPLEFT", 45, -217)
+		self:SetScale(0.85)
+	end)
 
---queuestatusframe
-hooksecurefunc(QueueStatusFrame, "UpdatePosition", function(self)
-	self:ClearAllPoints()
-	self:SetPoint("TOPRIGHT", QueueStatusButton, "TOPLEFT")
-end)
+	--queuestatusframe
+	hooksecurefunc(QueueStatusFrame, "UpdatePosition", function(self)
+		self:ClearAllPoints()
+		self:SetPoint("TOPRIGHT", QueueStatusButton, "TOPLEFT")
+	end)
+end
 
 --[[ Incompatibility Check ]]--
 
