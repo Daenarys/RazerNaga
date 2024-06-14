095f2441d733f4b8b1cefc3e80f7b026c9dec360
Daenarys <androidwoz@gmail.com>
2024-06-14 14:04:54 +0200

    cleanup

--- RazerNaga_Skin/skins/CurrencyTransfer.lua
+++ RazerNaga_Skin/skins/CurrencyTransfer.lua
@@ -1,16 +1,16 @@
-if _G.CurrencyTransferLog then
-	CurrencyTransferLog.ScrollBar:SetSize(25, 560)
-	CurrencyTransferLog.ScrollBar:ClearAllPoints()
-	CurrencyTransferLog.ScrollBar:SetPoint("TOPLEFT", CurrencyTransferLog.ScrollBox, "TOPRIGHT", -2, 2)
-	CurrencyTransferLog.ScrollBar:SetPoint("BOTTOMLEFT", CurrencyTransferLog.ScrollBox, "BOTTOMRIGHT", -2, -2)
+if not _G.CurrencyTransferLog then return end
 
-	if (CurrencyTransferLog.ScrollBar.Backplate == nil) then
-		CurrencyTransferLog.ScrollBar.Backplate = CurrencyTransferLog.ScrollBar:CreateTexture(nil, "BACKGROUND")
-		CurrencyTransferLog.ScrollBar.Backplate:SetColorTexture(0, 0, 0, 1)
-		CurrencyTransferLog.ScrollBar.Backplate:SetAllPoints()
-	end
+CurrencyTransferLog.ScrollBar:SetSize(25, 560)
+CurrencyTransferLog.ScrollBar:ClearAllPoints()
+CurrencyTransferLog.ScrollBar:SetPoint("TOPLEFT", CurrencyTransferLog.ScrollBox, "TOPRIGHT", -2, 2)
+CurrencyTransferLog.ScrollBar:SetPoint("BOTTOMLEFT", CurrencyTransferLog.ScrollBox, "BOTTOMRIGHT", -2, -2)
 
-	ApplyScrollBarArrow(CurrencyTransferLog.ScrollBar)
-	ApplyScrollBarTrack(CurrencyTransferLog.ScrollBar.Track)
-	ApplyScrollBarThumb(CurrencyTransferLog.ScrollBar.Track.Thumb)
+if (CurrencyTransferLog.ScrollBar.Backplate == nil) then
+	CurrencyTransferLog.ScrollBar.Backplate = CurrencyTransferLog.ScrollBar:CreateTexture(nil, "BACKGROUND")
+	CurrencyTransferLog.ScrollBar.Backplate:SetColorTexture(0, 0, 0, 1)
+	CurrencyTransferLog.ScrollBar.Backplate:SetAllPoints()
 end
+
+ApplyScrollBarArrow(CurrencyTransferLog.ScrollBar)
+ApplyScrollBarTrack(CurrencyTransferLog.ScrollBar.Track)
+ApplyScrollBarThumb(CurrencyTransferLog.ScrollBar.Track.Thumb)
