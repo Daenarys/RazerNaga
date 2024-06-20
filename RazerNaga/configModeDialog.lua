a3eb991d9154f4f9fde1d35e64d32e003d1e0d29
Daenarys <androidwoz@gmail.com>
2024-06-20 18:17:47 +0200

    cleanup

--- RazerNaga/configModeDialog.lua
+++ RazerNaga/configModeDialog.lua
@@ -6,7 +6,7 @@
 local RazerNaga = RazerNaga
 local L = LibStub('AceLocale-3.0'):GetLocale('RazerNaga')
 
-local ConfigModeDialog = CreateFrame('Frame', 'RazerNagaConfigHelperDialog', UIParent, BackdropTemplateMixin and 'BackdropTemplate')
+local ConfigModeDialog = CreateFrame('Frame', 'RazerNagaConfigHelperDialog', UIParent)
 ConfigModeDialog:SetPoint('TOP', 0, -24)
 ConfigModeDialog:SetScript('OnShow', function(self) self:Load() end)
 ConfigModeDialog:Hide()
@@ -21,23 +21,48 @@ function ConfigModeDialog:Load()
 	self:SetClampedToScreen(true)
 	self:SetWidth(360)
 	self:SetHeight(192)
-	self:SetBackdrop{
-		bgFile='Interface\\DialogFrame\\UI-DialogBox-Background' ,
-		edgeFile='Interface\\DialogFrame\\UI-DialogBox-Border',
-		tile = true,
-		insets = {left = 11, right = 12, top = 12, bottom = 11},
-		tileSize = 32,
-		edgeSize = 32,
-	}
 	self:RegisterForDrag('LeftButton')
 	self:SetScript('OnShow', function() PlaySound(SOUNDKIT.IG_MAINMENU_OPTION) end)
 	self:SetScript('OnHide', function() PlaySound(SOUNDKIT.GS_TITLE_OPTION_EXIT) end)
 	self:SetScript('OnDragStart', function() self:StartMoving() end)
 	self:SetScript('OnDragStop', function() self:StopMovingOrSizing() end)
 
+	local border = CreateFrame('Frame', nil, self, 'DialogBorderTemplate')
+	border.TopEdge:SetSize(32, 32)
+	border.TopEdge:SetTexture("Interface\\FrameGeneral\\UIFrameDiamondMetal", true)
+	border.TopEdge:SetTexCoord(0, 0.5, 0.13671875, 0.26171875)
+
+	border.TopLeftCorner:SetSize(32, 32)
+	border.TopLeftCorner:SetTexture("Interface\\FrameGeneral\\UIFrameDiamondMetal")
+	border.TopLeftCorner:SetTexCoord(0.015625, 0.515625, 0.53515625, 0.66015625)
+
+	border.TopRightCorner:SetSize(32, 32)
+	border.TopRightCorner:SetTexture("Interface\\FrameGeneral\\UIFrameDiamondMetal")
+	border.TopRightCorner:SetTexCoord(0.015625, 0.515625, 0.66796875, 0.79296875)
+
+	border.BottomEdge:SetSize(32, 32)
+	border.BottomEdge:SetTexture("Interface\\FrameGeneral\\UIFrameDiamondMetal", true)
+	border.BottomEdge:SetTexCoord(0, 0.5, 0.00390625, 0.12890625)
+
+	border.BottomLeftCorner:SetSize(32, 32)
+	border.BottomLeftCorner:SetTexture("Interface\\FrameGeneral\\UIFrameDiamondMetal")
+	border.BottomLeftCorner:SetTexCoord(0.015625, 0.515625, 0.26953125, 0.39453125)
+
+	border.BottomRightCorner:SetSize(32, 32)
+	border.BottomRightCorner:SetTexture("Interface\\FrameGeneral\\UIFrameDiamondMetal")
+	border.BottomRightCorner:SetTexCoord(0.015625, 0.515625, 0.40234375, 0.52734375)
+
+	border.LeftEdge:SetSize(32, 32)
+	border.LeftEdge:SetTexture("Interface\\FrameGeneral\\UIFrameDiamondMetalVertical", false, true)
+	border.LeftEdge:SetTexCoord(0.0078125, 0.2578125, 0, 1)
+
+	border.RightEdge:SetSize(32, 32)
+	border.RightEdge:SetTexture("Interface\\FrameGeneral\\UIFrameDiamondMetalVertical", false, true)
+	border.RightEdge:SetTexCoord(0.2734375, 0.5234375, 0, 1)
+
 	local header = self:CreateTexture(nil, 'ARTWORK')
-	header:SetTexture('Interface\\Addons\\RazerNaga\\Icons\\UI-DialogBox-Header')
-	header:SetSize(326, 64)
+	header:SetTexture('Interface\\DialogFrame\\UI-DialogBox-Header')
+	header:SetSize(326, 64) 
 	header:SetPoint('TOP', 0, 12)
 
 	local title = self:CreateFontString(nil, 'ARTWORK')
