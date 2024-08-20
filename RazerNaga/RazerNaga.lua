1532f2fcbad24515f46b6fcd19dbd4078801b61f
Daenarys <androidwoz@gmail.com>
2024-08-20 23:48:34 +0200

    cleanup

--- RazerNaga/RazerNaga.lua
+++ RazerNaga/RazerNaga.lua
@@ -924,40 +924,6 @@ if not (C_AddOns.IsAddOnLoaded("ClassicFrames")) then
 	end)
 end
 
---ui improvements
-if TextToSpeechButtonFrame then
-    TextToSpeechButtonFrame:Hide()
-end
-
-if QuickJoinToastButton then
-	QuickJoinToastButton:HookScript("OnUpdate", function(self)
-	    self:ClearAllPoints()
-	    self:SetPoint("BOTTOMLEFT", ChatAlertFrame, "BOTTOMLEFT")
-	end)
-end
-
-if FramerateFrame then
-	hooksecurefunc(FramerateFrame, "UpdatePosition", function(self)
-		self:ClearAllPoints()
-		self:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, 75)
-	end)
-end
-
-if ObjectiveTrackerFrame then
-	hooksecurefunc(ObjectiveTrackerFrame, "Update", function(self)
-		self.Header.MinimizeButton:SetNormalTexture("Interface/QuestFrame/QuestTracker")
-		self.Header.MinimizeButton:SetPushedTexture("Interface/QuestFrame/QuestTracker")
-		self.Header.MinimizeButton:SetHighlightAtlas("UI-QuestTrackerButton-Highlight", "ADD")
-		if self:IsCollapsed() then
-			self.Header.MinimizeButton:GetNormalTexture():SetTexCoord(0.933594, 0.96875, 0.242188, 0.316406)
-			self.Header.MinimizeButton:GetPushedTexture():SetTexCoord(0.894531, 0.929688, 0.324219, 0.398438)
-		else
-			self.Header.MinimizeButton:GetNormalTexture():SetTexCoord(0.9375, 0.972656, 0.121094, 0.195312)
-			self.Header.MinimizeButton:GetPushedTexture():SetTexCoord(0.894531, 0.929688, 0.242188, 0.316406)
-		end
-	end)
-end
-
 --[[ Incompatibility Check ]]--
 
 local INCOMPATIBLE_ADDONS = {
