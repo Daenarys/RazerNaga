f043cfa5073d82ad5f64ea17270212321b3456d1
Daenarys <androidwoz@gmail.com>
2024-11-28 15:39:53 +0100

    adjust elp button position

--- RazerNaga/RazerNaga.lua
+++ RazerNaga/RazerNaga.lua
@@ -912,6 +912,22 @@ end
 
 --queuestatus
 if not (C_AddOns.IsAddOnLoaded("ClassicFrames")) then
+	Minimap:HookScript("OnEvent", function(self, event, ...)
+		if ( event == "PLAYER_ENTERING_WORLD" ) then
+			if (ExpansionLandingPageMinimapButton:GetNormalTexture():GetAtlas() == "dragonflight-landingbutton-up") then
+				ExpansionLandingPageMinimapButton:ClearAllPoints()
+				ExpansionLandingPageMinimapButton:SetPoint("TOPLEFT", 8, -156)
+			end
+
+			hooksecurefunc(ExpansionLandingPageMinimapButton, "UpdateIcon", function(self)
+				if (self:GetNormalTexture():GetAtlas() == "dragonflight-landingbutton-up") then
+					self:ClearAllPoints()
+					self:SetPoint("TOPLEFT", 8, -156)
+				end
+			end)
+		end
+	end)
+
 	hooksecurefunc(QueueStatusButton, "UpdatePosition", function(self)
 		self:SetParent(MinimapBackdrop)
 		self:SetFrameLevel(6)
