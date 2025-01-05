dcf45f227b4c761b62ab59ca1693c068e4f5da5f
Daenarys <androidwoz@gmail.com>
2025-01-06 00:36:31 +0100

    disable grid funcs

--- RazerNaga/RazerNaga.lua
+++ RazerNaga/RazerNaga.lua
@@ -269,6 +269,9 @@ function RazerNaga:HideBlizzard()
 		"MultiBarLeft",
 		"MultiBarRight"
 	)
+
+	_G.MultiActionBar_HideAllGrids = function() end
+	_G.MultiActionBar_ShowAllGrids = function() end
 end
 
 --[[ Keybound Events ]]--
