bb5a422aa1ff14c5cd93992d544477c8e5a63282
Daenarys <androidwoz@gmail.com>
2025-01-05 15:49:44 +0100

    sync

--- RazerNaga_Cast/castBar.lua
+++ RazerNaga_Cast/castBar.lua
@@ -27,11 +27,8 @@ function CastBar:New()
 
 	if not f.cast then
 		f.cast = CastingBar:New(f)
-		f:SetWidth(240)
-		f:SetHeight(24)
 	end
 
-	f:UpdateText()
 	f:Layout()
 
 	return f
@@ -41,32 +38,13 @@ function CastBar:GetDefaults()
 	return {
 		point = 'CENTER',
 		x = 0,
-		y = 30,
-		showText = true
+		y = 30
 	}
-end
-
-function CastBar:ToggleText(enable)
-	self.sets.showText = enable or false
-	self:UpdateText()
-end
-
-function CastBar:UpdateText()
-	if self.sets.showText then
-		self.cast.Time:Show()
-	else
-		self.cast.Time:Hide()
-	end
-	self.cast:AdjustWidth()
 end
 
 function CastBar:CreateMenu()
 	local menu = RazerNaga:NewMenu(self.id)
 	local panel = menu:NewPanel(LibStub('AceLocale-3.0'):GetLocale('RazerNaga-Config').Layout)
-
-	local time = panel:NewCheckButton(RazerNaga_SHOW_TIME)
-	time:SetScript('OnClick', function(b) self:ToggleText(b:GetChecked()) end)
-	time:SetScript('OnShow', function(b) b:SetChecked(self.sets.showText) end)
 
 	panel:NewOpacitySlider()
 	panel:NewFadeSlider()
@@ -87,45 +65,9 @@ end
 
 CastingBar = RazerNaga:CreateClass('StatusBar')
 
-local BORDER_SCALE = 197/150
-local TEXT_PADDING = 18
-
 function CastingBar:New(parent)
 	local f = self:Bind(CreateFrame('StatusBar', 'RazerNagaCastingBar', parent, 'RazerNagaCastingBarTemplate'))
-	f:SetPoint('CENTER')
-
-	f.normalWidth = f:GetWidth()
-	f:SetScript('OnUpdate', f.OnUpdate)
+	f:SetPoint('CENTER', 0, 5)
 
 	return f
-end
-
-function CastingBar:OnUpdate(elapsed)
-	CastingBarFrame_OnUpdate(self, elapsed)
-
-	if self.casting then
-		self.Time:SetFormattedText('%.1f', self.maxValue - self.value)
-		self:AdjustWidth()
-	elseif self.channeling then
-		self.Time:SetFormattedText('%.1f', self.value)
-		self:AdjustWidth()
-	end
-end
-
-function CastingBar:AdjustWidth()
-	local textWidth = self.Text:GetStringWidth() + TEXT_PADDING
-	local timeWidth = (self.Time:IsShown() and (self.Time:GetStringWidth() + 4) * 2) or 0
-	local width = textWidth + timeWidth
-
-	if width < self.normalWidth then
-		width = self.normalWidth
-	end
-
-	local diff = math.abs(width - self:GetWidth())
-
-	if diff > TEXT_PADDING then
-		self:SetWidth(width)
-		self.Border:SetWidth(width * BORDER_SCALE)
-		self.Flash:SetWidth(width * BORDER_SCALE)
-	end
 end
