2ba897ac538596040f3a56031b797232ca7c3888
Daenarys <androidwoz@gmail.com>
2025-01-11 16:29:34 +0100

    sync

--- RazerNaga_Skin/skins/PVP.lua
+++
@@ -1,9 +0,0 @@
-PVPMatchScoreboard:HookScript("OnShow", function(self)
-	self:ClearAllPoints()
-	self:SetPoint("CENTER")
-end)
-
-PVPMatchResults:HookScript("OnShow", function(self)
-	self:ClearAllPoints()
-	self:SetPoint("CENTER")
-end)
