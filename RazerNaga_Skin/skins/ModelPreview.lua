7d86b3f8d42f9ffc1d801cd396bd2feb9bf829e7
Daenarys <androidwoz@gmail.com>
2025-01-14 13:06:44 +0100

    remove skin

--- RazerNaga_Skin/skins/ModelPreview.lua
+++
@@ -1,6 +0,0 @@
-if not _G.ModelPreviewFrame then return end
-
-ModelPreviewFrame.Display.ModelScene.ControlFrame:HookScript("OnShow", function(self)
-	self:ClearAllPoints()
-	self:SetPoint("TOP")
-end)
