3e375ef28356ce0430f563555fb9d9e9ad4c6ffd
Daenarys <androidwoz@gmail.com>
2025-04-17 15:36:53 +0200

    remove skin

--- RazerNaga_Skin/skins/Chat.lua
+++
@@ -1,8 +0,0 @@
--- hide default buttons
-TextToSpeechButtonFrame:Hide()
-
--- move down friends microbutton
-QuickJoinToastButton:HookScript("OnUpdate", function(self)
-    self:ClearAllPoints()
-    self:SetPoint("BOTTOMLEFT", ChatAlertFrame, "BOTTOMLEFT")
-end)
