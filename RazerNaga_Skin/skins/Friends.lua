4a30379cef7caeeec7f9a3e9f02e6ec40880e0b7
Daenarys <androidwoz@gmail.com>
2025-10-29 12:10:09 +0100

    add skin

---
+++ RazerNaga_Skin/skins/Friends.lua
@@ -0,0 +1,7 @@
+if not _G.FriendsFrame then return end
+
+FriendsFrame:SetWidth(338)
+WhoFrameColumnHeader1:SetWidth(83)
+
+FriendsFrameBattlenetFrame:ClearAllPoints()
+FriendsFrameBattlenetFrame:SetPoint("TOPLEFT", FriendsTabHeader, "TOPLEFT", 109, -26)
