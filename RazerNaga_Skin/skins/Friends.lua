bcf05985bfadd56f177044e4904c35d9a7e05d2a
Daenarys <androidwoz@gmail.com>
2025-10-29 12:34:43 +0100

    adjust tab size

--- RazerNaga_Skin/skins/Friends.lua
+++ RazerNaga_Skin/skins/Friends.lua
@@ -5,3 +5,7 @@ WhoFrameColumnHeader1:SetWidth(83)
 
 FriendsFrameBattlenetFrame:ClearAllPoints()
 FriendsFrameBattlenetFrame:SetPoint("TOPLEFT", FriendsTabHeader, "TOPLEFT", 109, -26)
+
+hooksecurefunc(FriendsTabHeader.TabSystem, 'Layout', function(self)
+	self.tabs[1]:SetWidth(36 + self.tabs[1]:GetFontString():GetStringWidth())
+end)
