9b4f51358c287517a4bfddf0fca87b1a0a3dea6d
Daenarys <androidwoz@gmail.com>
2025-12-03 10:48:07 +0100

    sync 7.x skin 11.2.7

--- RazerNaga/menuBar.lua
+++ RazerNaga/menuBar.lua
@@ -40,7 +40,287 @@ local MICRO_BUTTON_NAMES = {
 function MenuBar:SkinButton(button)
     if button.skinned then return end
 
-    button:SetSize(28, 36)
+    button:SetSize(28, 58)
+    button:SetHitRectInsets(0, 0, 18, 0)
+
+    local buttons = {
+        {button = CharacterMicroButton, name = "Character"},
+        {button = ProfessionMicroButton, name = "Spellbook"},
+        {button = PlayerSpellsMicroButton, name = "Talents"},
+        {button = AchievementMicroButton, name = "Achievement"},
+        {button = QuestLogMicroButton, name = "Quest"},
+        {button = GuildMicroButton, name = "Socials"},
+        {button = LFDMicroButton, name = "LFG"},
+        {button = CollectionsMicroButton, name = "Mounts"},
+        {button = EJMicroButton, name = "EJ"},
+        {button = StoreMicroButton, name = "BStore"},  
+        {button = MainMenuMicroButton, name = "MainMenu"}
+    }
+
+    local function replaceAtlases(self, name)
+        local prefix = "Interface\\Buttons\\UI-MicroButton-"
+        self:SetNormalTexture(prefix..name.."-Up")
+        self:SetPushedTexture(prefix..name.."-Down")
+        if self:GetDisabledTexture() then
+            self:SetDisabledTexture(prefix..name.."-Disabled")
+        end
+    end
+
+    local function replaceAllAtlases()
+        for _, data in pairs(buttons) do
+            replaceAtlases(data.button, data.name)
+        end
+    end
+    replaceAllAtlases()
+
+    button:HookScript("OnUpdate", function(self)
+        local normalTexture = self:GetNormalTexture()
+        if (normalTexture) then
+            normalTexture:SetAlpha(1)
+            normalTexture:SetTexelSnappingBias(0.0)
+        end
+        local pushedTexture = self:GetPushedTexture()
+        if (pushedTexture) then
+            pushedTexture:SetTexelSnappingBias(0.0)
+        end
+        local disabledTexture = self:GetDisabledTexture()
+        if (disabledTexture) then
+            disabledTexture:SetTexelSnappingBias(0.0)
+        end
+        local highlightTexture = self:GetHighlightTexture()
+        if (highlightTexture) then
+            highlightTexture:SetAlpha(1)
+            highlightTexture:SetTexelSnappingBias(0.0)
+        end
+        if self.Background then
+            self.Background:Hide()
+        end
+        if self.PushedBackground then
+            self.PushedBackground:Hide()
+        end
+        if self.Shadow then
+            self.Shadow:Hide()
+        end
+        if self.PushedShadow then
+            self.PushedShadow:Hide()
+        end
+        if self.FlashBorder then
+            self.FlashBorder:SetSize(64, 64)
+            self.FlashBorder:SetTexture("Interface\\Buttons\\Micro-Highlight")
+            self.FlashBorder:ClearAllPoints()
+            self.FlashBorder:SetPoint("TOPLEFT", -2, -18)
+        end
+        if self.FlashContent then
+            UIFrameFlashStop(self.FlashContent)
+        end
+        if self.Emblem then
+            self.Emblem:Hide()
+        end
+        if self.HighlightEmblem then
+            self.HighlightEmblem:Hide()
+        end
+        self:SetHighlightTexture("Interface\\Buttons\\UI-MicroButton-Hilight")
+    end)
+
+    CharacterMicroButton:SetNormalTexture("Interface\\Buttons\\UI-MicroButtonCharacter-Up")
+    CharacterMicroButton:SetPushedTexture("Interface\\Buttons\\UI-MicroButtonCharacter-Down")
+    CharacterMicroButton:SetHighlightTexture("Interface\\Buttons\\UI-MicroButton-Hilight")
+
+    if not MicroButtonPortrait then
+        local portrait = CharacterMicroButton:CreateTexture("MicroButtonPortrait", "OVERLAY")
+        portrait:SetSize(18, 25)
+        portrait:SetPoint("TOP", 0, -28)
+        portrait:SetTexCoord(0.2, 0.8, 0.0666, 0.9)
+    end
+
+    CharacterMicroButton:HookScript("OnEvent", function(self, event, ...)
+        if ( event == "UNIT_PORTRAIT_UPDATE" ) then
+            local unit = ...;
+            if ( unit == "player" ) then
+                SetPortraitTexture(MicroButtonPortrait, "player")
+            end
+        elseif ( event == "PORTRAITS_UPDATED" ) then
+            SetPortraitTexture(MicroButtonPortrait, "player")
+        elseif ( event == "PLAYER_ENTERING_WORLD" ) then
+            SetPortraitTexture(MicroButtonPortrait, "player")
+        end
+    end)
+
+    local function CharacterMicroButton_SetPushed()
+        MicroButtonPortrait:SetTexCoord(0.2666, 0.8666, 0, 0.8333)
+        MicroButtonPortrait:SetAlpha(0.5)
+    end
+
+    local function CharacterMicroButton_SetNormal()
+        MicroButtonPortrait:SetTexCoord(0.2, 0.8, 0.0666, 0.9)
+        MicroButtonPortrait:SetAlpha(1.0)
+    end
+
+    CharacterMicroButton:HookScript("OnMouseDown", function(self)
+        if ( not KeybindFrames_InQuickKeybindMode() and self:IsEnabled() ) then
+            MicroButtonPortrait:SetTexCoord(0.2666, 0.8666, 0, 0.8333)
+            MicroButtonPortrait:SetAlpha(0.5)
+        end
+    end)
+
+    CharacterMicroButton:HookScript("OnMouseUp", function(self)
+        if ( not KeybindFrames_InQuickKeybindMode() and self:IsEnabled() ) then
+            MicroButtonPortrait:SetTexCoord(0.2, 0.8, 0.0666, 0.9)
+            MicroButtonPortrait:SetAlpha(1.0)
+        end
+    end)
+
+    if not MainMenuBarDownload then
+        MainMenuBarDownload = MainMenuMicroButton:CreateTexture("MainMenuBarDownload", "OVERLAY")
+        MainMenuBarDownload:SetSize(28, 28)
+        MainMenuBarDownload:SetPoint("BOTTOM", MainMenuMicroButton, "BOTTOM", 0, -7)
+    end
+    
+    MainMenuMicroButton:HookScript("OnUpdate", function(self, elapsed)
+        local status = GetFileStreamingStatus();
+        if ( status == 0 ) then
+            MainMenuBarDownload:Hide()
+            self:SetNormalTexture("Interface\\Buttons\\UI-MicroButton-MainMenu-Up")
+            self:SetPushedTexture("Interface\\Buttons\\UI-MicroButton-MainMenu-Down")
+            self:SetDisabledTexture("Interface\\Buttons\\UI-MicroButton-MainMenu-Disabled")
+        else
+            self:SetNormalTexture("Interface\\Buttons\\UI-MicroButtonStreamDL-Up")
+            self:SetPushedTexture("Interface\\Buttons\\UI-MicroButtonStreamDL-Down")
+            self:SetDisabledTexture("Interface\\Buttons\\UI-MicroButtonStreamDL-Up")
+        if ( status == 1 ) then
+            MainMenuBarDownload:SetTexture("Interface\\BUTTONS\\UI-MicroStream-Green")
+        elseif ( status == 2 ) then
+            MainMenuBarDownload:SetTexture("Interface\\BUTTONS\\UI-MicroStream-Yellow")
+        elseif ( status == 3 ) then
+            MainMenuBarDownload:SetTexture("Interface\\BUTTONS\\UI-MicroStream-Red")
+        end
+            MainMenuBarDownload:Show()
+        end
+    end)
+
+    hooksecurefunc(MainMenuMicroButton, "UpdateNotificationIcon", function(self)
+        self.NotificationOverlay:Hide()
+    end)
+
+    if not GuildMicroButtonTabard then
+        local GuildMicroButtonTabard = CreateFrame("Frame", "GuildMicroButtonTabard", GuildMicroButton)
+        GuildMicroButtonTabard:SetSize(28, 58)
+        GuildMicroButtonTabard:SetPoint("TOPLEFT")
+        GuildMicroButtonTabard:Hide()
+    end
+
+    if not GuildMicroButtonTabardBackground then
+        GuildMicroButtonTabard.background = GuildMicroButtonTabard:CreateTexture("GuildMicroButtonTabardBackground", "ARTWORK")
+        GuildMicroButtonTabardBackground:SetSize(30, 60)
+        GuildMicroButtonTabardBackground:SetTexture("Interface\\Buttons\\UI-MicroButton-Guild-Banner")
+        GuildMicroButtonTabardBackground:SetPoint("CENTER", 0, 0)
+    end
+
+    if not GuildMicroButtonTabardEmblem then
+        GuildMicroButtonTabard.emblem = GuildMicroButtonTabard:CreateTexture("GuildMicroButtonTabardEmblem", "OVERLAY")
+        GuildMicroButtonTabardEmblem:SetSize(16, 16)
+        GuildMicroButtonTabardEmblem:SetTexture("Interface\\GuildFrame\\GuildEmblems_01")
+        GuildMicroButtonTabardEmblem:SetPoint("CENTER", 0, -9)
+    end
+
+    GuildMicroButton:HookScript("OnMouseDown", function(self)
+        if self:IsEnabled() then
+            GuildMicroButtonTabard:SetPoint("TOPLEFT", -1, -1)
+            GuildMicroButtonTabard:SetAlpha(0.5)
+        end
+    end)
+
+    GuildMicroButton:HookScript("OnMouseUp", function(self)
+        if self:IsEnabled() then
+            GuildMicroButtonTabard:SetPoint("TOPLEFT", 0, 0)
+            GuildMicroButtonTabard:SetAlpha(1.0)
+        end
+    end)
+
+    hooksecurefunc(GuildMicroButton, "UpdateNotificationIcon", function(self)
+        self.NotificationOverlay:Hide()
+    end)
+
+    hooksecurefunc(GuildMicroButton, "UpdateTabard", function()
+        local tabard = GuildMicroButtonTabard;
+        if ( not tabard.needsUpdate ) then
+            return;
+        end
+        -- switch textures if the guild has a custom tabard
+        local emblemFilename = select(10, GetGuildLogoInfo())
+        if ( emblemFilename ) then
+            if ( not tabard:IsShown() ) then
+                local button = GuildMicroButton;
+                button:SetNormalTexture("Interface\\Buttons\\UI-MicroButtonCharacter-Up")
+                button:SetPushedTexture("Interface\\Buttons\\UI-MicroButtonCharacter-Down")
+                button:SetDisabledTexture("Interface\\Buttons\\UI-MicroButtonCharacter-Up")
+                tabard:Show()
+            end
+            SetSmallGuildTabardTextures("player", tabard.emblem, tabard.background)
+        else
+            if ( tabard:IsShown() ) then
+                local button = GuildMicroButton;
+                button:SetNormalTexture("Interface\\Buttons\\UI-MicroButton-Socials-Up")
+                button:SetPushedTexture("Interface\\Buttons\\UI-MicroButton-Socials-Down")
+                button:SetDisabledTexture("Interface\\Buttons\\UI-MicroButton-Socials-Disabled")
+                tabard:Hide()
+            end
+        end
+        tabard.needsUpdate = nil;
+    end)
+
+    GuildMicroButton:HookScript("OnEvent", function(self, event, ...)
+        if ( event == "PLAYER_ENTERING_WORLD" or event == "PLAYER_GUILD_UPDATE" or event == "NEUTRAL_FACTION_SELECT_RESULT" ) then
+            GuildMicroButtonTabard.needsUpdate = true;
+        end
+    end)
+
+    hooksecurefunc("UpdateMicroButtons", function()
+        if AchievementMicroButton:IsEnabled() then
+            AchievementMicroButton.tooltipText = MicroButtonTooltipText(ACHIEVEMENT_BUTTON, "TOGGLEACHIEVEMENT");
+        end
+        if CharacterMicroButton.Portrait then
+            CharacterMicroButton.Portrait:Hide()
+        end
+        if CharacterMicroButton.PortraitMask then
+            CharacterMicroButton.PortraitMask:Hide()
+        end
+        if ( CharacterFrame and CharacterFrame:IsShown() ) then
+            CharacterMicroButton_SetPushed()
+        else
+            CharacterMicroButton_SetNormal()
+        end
+        if not CharacterMicroButton:IsEnabled() then
+            SetDesaturation(MicroButtonPortrait, true)
+        else
+            SetDesaturation(MicroButtonPortrait, false)
+        end
+        GuildMicroButton:GetNormalTexture():SetVertexColor(1, 1, 1)
+        GuildMicroButton:GetPushedTexture():SetVertexColor(1, 1, 1)
+        GuildMicroButton:GetDisabledTexture():SetVertexColor(1, 1, 1)
+        GuildMicroButton:GetHighlightTexture():SetVertexColor(1, 1, 1)
+        if ( CommunitiesFrame and CommunitiesFrame:IsShown() ) or ( GuildFrame and GuildFrame:IsShown() ) then
+            GuildMicroButtonTabard:SetPoint("TOPLEFT", -1, -1)
+            GuildMicroButtonTabard:SetAlpha(0.70)
+        else
+            GuildMicroButtonTabard:SetPoint("TOPLEFT", 0, 0)
+            GuildMicroButtonTabard:SetAlpha(1)
+        end
+    end)
+
+    hooksecurefunc("LoadMicroButtonTextures", function()
+        local button = GuildMicroButton
+        local emblemFilename = select(10, GetGuildLogoInfo())
+        if ( emblemFilename ) then
+            button:SetNormalTexture("Interface\\Buttons\\UI-MicroButtonCharacter-Up")
+            button:SetPushedTexture("Interface\\Buttons\\UI-MicroButtonCharacter-Down")
+            button:SetDisabledTexture("Interface\\Buttons\\UI-MicroButtonCharacter-Up")
+        else
+            button:SetNormalTexture("Interface\\Buttons\\UI-MicroButton-Socials-Up")
+            button:SetPushedTexture("Interface\\Buttons\\UI-MicroButton-Socials-Down")
+            button:SetDisabledTexture("Interface\\Buttons\\UI-MicroButton-Socials-Disabled")
+        end
+    end)
 
     hooksecurefunc("HelpOpenWebTicketButton_OnUpdate", function(self)
         self:SetParent(MainMenuMicroButton)
@@ -225,7 +505,7 @@ function MenuBar:LayoutNormal()
 
     local firstButton = self.buttons[1]
     local w = firstButton:GetWidth() + spacing - 2
-    local h = firstButton:GetHeight() + spacing - 1
+    local h = firstButton:GetHeight() + spacing - 20
 
     for i, button in pairs(self.activeButtons) do
         local col, row
@@ -244,7 +524,7 @@ function MenuBar:LayoutNormal()
         
         button:SetParent(self)
         button:ClearAllPoints()
-        button:SetPoint('TOPLEFT', w*col + pW, -(h*row + pH) + 1)
+        button:SetPoint('TOPLEFT', w*col + pW, -(h*row + pH) + 20)
         button:Show()
     end
 
@@ -278,11 +558,11 @@ function MenuBar:FixButtonPositions()
         button:SetParent(PetBattleFrame.BottomFrame.MicroButtonFrame)
         button:ClearAllPoints()
         if i == 1 then
-            button:SetPoint('TOPLEFT', -4, 3)
+            button:SetPoint('TOPLEFT', -12, 25)
         elseif i == 7 then
-            button:SetPoint('TOPLEFT', self.overrideButtons[1], 'BOTTOMLEFT', 0, 6)
+            button:SetPoint('TOPLEFT', self.overrideButtons[1], 'BOTTOMLEFT', 0, 25)
         else
-            button:SetPoint('TOPLEFT', self.overrideButtons[i - 1], 'TOPRIGHT', -5, 0)
+            button:SetPoint('TOPLEFT', self.overrideButtons[i - 1], 'TOPRIGHT', 0, 0)
         end
 
         button:Show()
@@ -382,6 +662,8 @@ function MenuBarModule:OnInitialize()
     local perf = MainMenuMicroButton and MainMenuMicroButton.MainMenuBarPerformanceBar
     if perf then
         perf:SetSize(28, 58)
+        perf:ClearAllPoints()
+        perf:SetPoint('CENTER')
     end
 
     if FramerateFrame then
--- RazerNaga/petBar.lua
+++ RazerNaga/petBar.lua
@@ -8,23 +8,75 @@ local RazerNaga = _G[...]
 -- Button
 --------------------------------------------------------------------------------
 
-local PetButtons = setmetatable({}, {
-    __index = function(self, index)
-        local button =  (PetActionBar and PetActionBar.actionButtons[index])
-            or _G['PetActionButton' .. index]
+local function getPetButton(id)
+    return _G[('PetActionButton%d'):format(id)]
+end
 
-        if button then
-            button.buttonType = 'BONUSACTIONBUTTON'
-            button:SetAttribute("commandName", "BONUSACTIONBUTTON" .. index)
-            RazerNaga.BindableButton:AddQuickBindingSupport(button)
-            button.cooldown:SetDrawBling(true)
-
-            self[index] = button
-        end
-
-        return button
+local function skinPetButton(self)
+    _G[self:GetName() .. 'Icon']:SetTexCoord(0.06, 0.94, 0.06, 0.94)
+    self.IconMask:SetSize(64, 64)
+    self.SlotBackground:Hide()
+    self.PushedTexture:SetTexture([[Interface\Buttons\UI-Quickslot-Depress]])
+    self.PushedTexture:ClearAllPoints()
+    self.PushedTexture:SetAllPoints()
+    self.HighlightTexture:SetTexture([[Interface\Buttons\ButtonHilight-Square]])
+    self.HighlightTexture:ClearAllPoints()
+    self.HighlightTexture:SetAllPoints()
+    self.HighlightTexture:SetBlendMode("ADD")
+    self.CheckedTexture:SetTexture([[Interface\Buttons\CheckButtonHilight]])
+    self.CheckedTexture:ClearAllPoints()
+    self.CheckedTexture:SetAllPoints()
+    self.CheckedTexture:SetBlendMode("ADD")
+    self.Flash:SetTexture([[Interface\Buttons\UI-QuickslotRed]])
+    self.Flash:ClearAllPoints()
+    self.Flash:SetAllPoints()
+    self.AutoCastOverlay.Mask:SetPoint("TOPLEFT")
+    self.AutoCastOverlay.Mask:SetPoint("BOTTOMRIGHT")
+    if self.IconMask then
+        self.IconMask:Hide()
     end
-})
+
+    --simulate old floatingbg
+    hooksecurefunc(PetActionBar, 'Update', function()
+        local petActionID = self:GetID()
+        local texture = GetPetActionInfo(petActionID);
+        if ( texture ) then
+            self.NormalTexture:SetTexture([[Interface\Buttons\UI-Quickslot2]])
+            self.NormalTexture:SetSize(54, 54)
+            self.NormalTexture:ClearAllPoints()
+            self.NormalTexture:SetPoint("CENTER", 0, -1)
+            self.NormalTexture:SetVertexColor(1, 1, 1, 0.5)
+        else
+            self.NormalTexture:SetTexture([[Interface\Buttons\UI-Quickslot]])
+            self.NormalTexture:SetSize(54, 54)
+            self.NormalTexture:ClearAllPoints()
+            self.NormalTexture:SetPoint("CENTER", 0, -1)
+            self.NormalTexture:SetVertexColor(1, 1, 1, 0.5)
+        end
+    end)
+end
+
+for id = 1, NUM_PET_ACTION_SLOTS do
+    local button = getPetButton(id)
+
+    -- set the buttontype
+    button.buttonType = 'BONUSACTIONBUTTON'
+    button:SetAttribute("commandName", "BONUSACTIONBUTTON" .. id)
+
+    -- apply hooks for quick binding
+    RazerNaga.BindableButton:AddQuickBindingSupport(button)
+
+    -- disable new texture loading
+    if button.UpdateButtonArt then
+        button.UpdateButtonArt = function() end
+    end
+
+    -- apply pre 10.x button skin
+    skinPetButton(button)
+
+    -- enable cooldown bling
+    button.cooldown:SetDrawBling(true)
+end
 
 --------------------------------------------------------------------------------
 -- Bar
@@ -37,12 +89,14 @@ function PetBar:New()
 end
 
 function PetBar:GetShowStates()
-    return '[@pet,exists,novehicleui,nooverridebar,nopossessbar]show;hide'
+    return '[@pet,exists,nopossessbar]show;hide'
 end
 
 function PetBar:GetDefaults()
     return {
         point = 'CENTER',
+        x = 0,
+        y = -32,
         spacing = 6
     }
 end
@@ -52,7 +106,7 @@ function PetBar:NumButtons()
 end
 
 function PetBar:AcquireButton(index)
-    return PetButtons[index]
+    return getPetButton(index)
 end
 
 function PetBar:OnAttachButton(button)
@@ -69,11 +123,14 @@ function PetBar:KEYBOUND_ENABLED()
 end
 
 function PetBar:KEYBOUND_DISABLED()
-    local hasPetBar = PetHasActionBar()
+    local petBarShown = PetHasActionBar()
 
     for _, button in pairs(self.buttons) do
-        local show = hasPetBar and GetPetActionInfo(button.index or button:GetID())
-        button:SetShown(show)
+        if petBarShown and GetPetActionInfo(button:GetID()) then
+            button:Show()
+        else
+            button:Hide()
+        end
     end
 end
 
@@ -81,15 +138,23 @@ end
 -- Module
 --------------------------------------------------------------------------------
 
-local PetBarModule = RazerNaga:NewModule('PetBar')
+local PetBarModule = RazerNaga:NewModule('PetBar', 'AceEvent-3.0')
 
 function PetBarModule:Load()
     self.bar = PetBar:New()
+
+    self:RegisterEvent('UPDATE_BINDINGS')
 end
 
 function PetBarModule:Unload()
+    self:UnregisterAllEvents()
+
     if self.bar then
         self.bar:Free()
         self.bar = nil
     end
+end
+
+function PetBarModule:UPDATE_BINDINGS()
+    self.bar:ForButtons('UpdateHotkeys')
 end
