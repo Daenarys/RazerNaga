b13965e312b13b340f3739c47c75963eb425d07a
Daenarys <androidwoz@gmail.com>
2025-12-15 21:11:04 +0100

    friends skin improvements

--- RazerNaga_Skin/skins/Friends.lua
+++ RazerNaga_Skin/skins/Friends.lua
@@ -1,10 +1,14 @@
 if not _G.FriendsFrame then return end
 
 FriendsFrame:SetWidth(338)
-WhoFrameColumnHeader1:SetWidth(83)
+WhoFrameColumnHeader4Middle:SetWidth(48)
 
 FriendsFrameBattlenetFrame:ClearAllPoints()
 FriendsFrameBattlenetFrame:SetPoint("TOPLEFT", FriendsTabHeader, "TOPLEFT", 109, -26)
+
+FriendsFrameBattlenetFrame.ContactsMenuButton.Icon:Hide()
+FriendsFrameBattlenetFrame.ContactsMenuButton:SetNormalTexture("Interface\\FriendsFrame\\broadcast-normal")
+FriendsFrameBattlenetFrame.ContactsMenuButton:SetPushedTexture("Interface\\FriendsFrame\\broadcast-press")
 
 hooksecurefunc(FriendsTabHeader.TabSystem, 'Layout', function(self)
 	self.tabs[1]:SetWidth(36 + self.tabs[1]:GetFontString():GetStringWidth())
