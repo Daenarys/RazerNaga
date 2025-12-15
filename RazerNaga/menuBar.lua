9fa25a46835fcb8337a17b798c49261296bd215d
Daenarys <androidwoz@gmail.com>
2025-12-15 20:54:40 +0100

    sync 10.x skin 11.2.7

--- RazerNaga/menuBar.lua
+++ RazerNaga/menuBar.lua
@@ -15,7 +15,6 @@ local MICRO_BUTTONS = {
     "PlayerSpellsMicroButton",
     "AchievementMicroButton",
     "QuestLogMicroButton",
-    "HousingMicroButton",
     "GuildMicroButton",
     "LFDMicroButton",
     "CollectionsMicroButton",
@@ -30,7 +29,6 @@ local MICRO_BUTTON_NAMES = {
     ['PlayerSpellsMicroButton'] = _G['TALENTS_BUTTON'],
     ['AchievementMicroButton'] = _G['ACHIEVEMENT_BUTTON'],
     ['QuestLogMicroButton'] = _G['QUESTLOG_BUTTON'],
-    ['HousingMicroButton'] = _G['HOUSING_MICRO_BUTTON'],
     ['GuildMicroButton'] = _G['GUILD_AND_COMMUNITIES'],
     ['LFDMicroButton'] = _G['DUNGEONS_BUTTON'],
     ['CollectionsMicroButton'] = _G['COLLECTIONS'],
@@ -43,6 +41,8 @@ function MenuBar:SkinButton(button)
     if button.skinned then return end
 
     button:SetSize(28, 36)
+
+    LoadMicroButtonTextures(ProfessionMicroButton, "SpellbookAbilities")
 
     hooksecurefunc("HelpOpenWebTicketButton_OnUpdate", function(self)
         self:SetParent(MainMenuMicroButton)
