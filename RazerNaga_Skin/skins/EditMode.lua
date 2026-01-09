47b7883e1b62d86fbfda3eadded8121648146c41
Daenarys <androidwoz@gmail.com>
2026-01-09 19:36:45 +0100

    dropdown improvements

--- RazerNaga_Skin/skins/EditMode.lua
+++ RazerNaga_Skin/skins/EditMode.lua
@@ -1,6 +1,10 @@
 if not _G.EditModeManagerFrame then return end
 
 ApplyDropDown(EditModeManagerFrame.LayoutDropdown)
+EditModeManagerFrame.LayoutDropdown:SetWidth(215)
+EditModeManagerFrame.LayoutDropdown:SetPoint("TOPLEFT", 23, -50)
+
+EditModeManagerFrame.LayoutLabel:SetPoint("BOTTOMLEFT", EditModeManagerFrame.LayoutDropdown, "TOPLEFT", 1, -2)
 
 EditModeSystemSettingsDialog:HookScript("OnShow", function(self)
 	for _, frame in next, { self.Settings:GetChildren() } do
