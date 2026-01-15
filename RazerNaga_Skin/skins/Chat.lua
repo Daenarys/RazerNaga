5937de4c635ed37fd271db4d2d573d519fae4164
Daenarys <androidwoz@gmail.com>
2026-01-15 18:18:30 +0100

    sync 7.x skin 11.2.7

--- RazerNaga_Skin/skins/Chat.lua
+++
@@ -1,6 +0,0 @@
-for i = 1, NUM_CHAT_WINDOWS do
-    hooksecurefunc(_G["ChatFrame"..i].editBox, "UpdateNewcomerEditBoxHint", function(self)
-        self.NewcomerHint:SetShown(false)
-        self:SetAlpha(0.25)
-    end)
-end
--- RazerNaga_Skin/skins/ItemUpgrade.lua
+++
@@ -1,7 +0,0 @@
-local f = CreateFrame("Frame")
-f:RegisterEvent("ADDON_LOADED")
-f:SetScript("OnEvent", function(self, event, name)
-	if name == "Blizzard_ItemUpgradeUI" then
-		ApplyDropDown(ItemUpgradeFrame.ItemInfo.Dropdown)
-	end
-end)
