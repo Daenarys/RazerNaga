b630e65215572cf319371722c9e75a93a460c161
Daenarys <androidwoz@gmail.com>
2026-02-07 17:28:30 +0100

    move text to right

--- RazerNaga_Skin/skins/Transmog.lua
+++ RazerNaga_Skin/skins/Transmog.lua
@@ -2,8 +2,8 @@ local f = CreateFrame("Frame")
 f:RegisterEvent("ADDON_LOADED")
 f:SetScript("OnEvent", function(self, event, name)
 	if name == "Blizzard_Transmog" then
-		ApplyDropDown(TransmogFrame.OutfitPopup.BorderBox.IconTypeDropdown)
 		ApplyDropDown(TransmogFrame.WardrobeCollection.TabContent.ItemsFrame.WeaponDropdown)
+		TransmogFrame.WardrobeCollection.TabContent.ItemsFrame.WeaponDropdown.Text:SetJustifyH("RIGHT")
 
 		hooksecurefunc(TransmogFrame.WardrobeCollection.TabContent.SituationsFrame, "Refresh", function(frame)
 			for situationFrame in TransmogFrame.WardrobeCollection.TabContent.SituationsFrame.SituationFramePool:EnumerateActive() do
@@ -13,5 +13,7 @@ f:SetScript("OnEvent", function(self, event, name)
 
 		ApplyFilterDropDown(TransmogFrame.WardrobeCollection.TabContent.ItemsFrame.FilterButton)
 		ApplyFilterDropDown(TransmogFrame.WardrobeCollection.TabContent.SetsFrame.FilterButton)
+
+		ApplyDropDown(TransmogFrame.OutfitPopup.BorderBox.IconTypeDropdown)
 	end
 end)
