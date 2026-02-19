349bba0bc9bc12490e2dd4bd5aea59cf46b44119
Daenarys <androidwoz@gmail.com>
2026-02-19 11:59:18 +0100

    sync 12.x skin 12.0.1

--- RazerNaga_Skin/skins/GroupLoot.lua
+++
@@ -1,3 +0,0 @@
-if not _G.GroupLootHistoryFrame then return end
-
-ApplyDropDown(GroupLootHistoryFrame.EncounterDropdown)
--- RazerNaga_Skin/skins/Professions.lua
+++
@@ -1,8 +0,0 @@
-local f = CreateFrame("Frame")
-f:RegisterEvent("ADDON_LOADED")
-f:SetScript("OnEvent", function(self, event, name)
-	if name == "Blizzard_Professions" then
-		ApplyFilterDropDown(ProfessionsFrame.CraftingPage.RecipeList.FilterDropdown)
-		ApplyFilterDropDown(ProfessionsFrame.OrdersPage.BrowseFrame.RecipeList.FilterDropdown)
-	end
-end)
