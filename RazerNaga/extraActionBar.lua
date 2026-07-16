0bbc95af6d83bcdedd630bad5130b654ed0a7b02
Daenarys <androidwoz@gmail.com>
2026-07-16 10:07:04 +0200

    fix

--- RazerNaga/extraActionBar.lua
+++ RazerNaga/extraActionBar.lua
@@ -1,36 +1,90 @@
-local ExtraAbilityContainer = _G.ExtraAbilityContainer
-if not ExtraAbilityContainer then
+local ExtraActionBarFrame = _G.ExtraActionBarFrame
+if not ExtraActionBarFrame then
     return
 end
 
 local RazerNaga = _G.RazerNaga
+local KeyBound = LibStub('LibKeyBound-1.0')
+local Tooltips = RazerNaga:GetModule('Tooltips')
+local Bindings = RazerNaga.BindingsController
+
+--[[ buttons ]]--
+
+local ExtraActionButton = RazerNaga:CreateClass('CheckButton', RazerNaga.BindableButton)
+
+do
+	local unused = {}
+
+	function ExtraActionButton:New(id)
+		local button = self:Restore(id) or self:Create(id)
+
+		Tooltips:Register(button)
+		Bindings:Register(button)
+
+		return button
+	end
+
+	function ExtraActionButton:Create(id)
+		local b = self:Bind(_G[('ExtraActionButton%d'):format(id)])
+
+		if b then
+			b.buttonType = 'EXTRAACTIONBUTTON'
+			b:HookScript('OnEnter', self.OnEnter)
+			b:Skin()
+
+			return b
+		end
+	end
+
+	--if we have button facade support, then skin the button that way
+	--otherwise, apply the RazerNaga style to the button to make it pretty
+	function ExtraActionButton:Skin()
+		if not RazerNaga:Masque('Extra Bar', self) then
+			self.icon:SetTexCoord(0.06, 0.94, 0.06, 0.94)
+			self:GetNormalTexture():SetVertexColor(1, 1, 1, 0.5)
+		end
+	end
+
+	function ExtraActionButton:Restore(id)
+		local b = unused and unused[id]
+
+		if b then
+			unused[id] = nil
+			b:Show()
+
+			return b
+		end
+	end
+
+	--saving them thar memories
+	function ExtraActionButton:Free()
+		unused[self:GetID()] = self
+
+		self:SetParent(nil)
+		self:Hide()
+
+		Tooltips:Unregister(button)
+		Bindings:Unregister(self)
+	end
+
+	--keybound support
+	function ExtraActionButton:OnEnter()
+		KeyBound:Set(self)
+	end
+end
+
 
 --[[ bar ]]--
 
 local ExtraBar = RazerNaga:CreateClass('Frame', RazerNaga.Frame)
 
 function ExtraBar:New()
-	local frame = RazerNaga.Frame.New(self, 'extra')
+	local f = RazerNaga.Frame.New(self, 'extra')
+	
+	f:LoadButtons()
+	f:Layout()
 
-	-- attach the ExtraAbilityContainer to the bar
-	local container = ExtraAbilityContainer
-
-	container:ClearAllPoints()
-	container:SetPoint('CENTER', frame.header)
-	container:SetParent(frame.header)
-	container:SetToplevel(false)
-
-	frame.container = container
-
-	-- drop need for showstates for this case, as the extra bar can show in more
-	-- conditions with shadowlands
-    if frame:GetShowStates() == '[extrabar]show;hide' then
-        frame:SetShowStates(nil)
-    end
-
-	frame:Layout()
-
-	return frame
+	return f
 end
 
 function ExtraBar:GetDefaults()
@@ -41,32 +95,35 @@ function ExtraBar:GetDefaults()
 	}
 end
 
-function ExtraBar:Layout()
-    local w, h = 256, 120
-    local pW, pH = self:GetPadding()
-
-    self:SetSize(w + pW, h + pH)
+function ExtraBar:GetShowStates()
+	return '[extrabar]show;hide'
 end
 
-function ExtraBar:CreateMenu()
-	local menu = RazerNaga:NewMenu(self.id)
-
-	self:AddLayoutPanel(menu)
-
-	self.menu = menu
-
-	return menu
+function ExtraBar:NumButtons()
+	return 1
 end
 
-function ExtraBar:AddLayoutPanel(menu)
-	local panel = menu:NewPanel(LibStub('AceLocale-3.0'):GetLocale('RazerNaga-Config').Layout)
+function ExtraBar:AddButton(i)
+	local b = ExtraActionButton:New(i)
 
-	panel.opacitySlider = panel:NewOpacitySlider()
-	panel.fadeSlider = panel:NewFadeSlider()
-	panel.scaleSlider = panel:NewScaleSlider()
-	panel.paddingSlider = panel:NewPaddingSlider()
+	if b then
+		b:SetAttribute('showgrid', 1)
+		b:SetParent(self.header)
+		b:Show()
 
-	return panel
+		self.buttons[i] = b
+	end
+end
+
+function ExtraBar:RemoveButton(i)
+	local b = self.buttons[i]
+
+	if b then
+		b:SetParent(nil)
+		b:Hide()
+
+		self.buttons[i] = nil
+	end
 end
 
 
@@ -75,45 +132,13 @@ end
 local ExtraBarController = RazerNaga:NewModule('ExtraBar')
 
 function ExtraBarController:OnInitialize()
-	-- disable mouse interactions on the extra action bar
-	-- as it can sometimes block the UI from being interactive
-	if ExtraActionBarFrame:IsMouseEnabled() then
-		ExtraActionBarFrame:EnableMouse(false)
-	end
-
-	-- prevent the stock UI from messing with the extra ability bar position
-	ExtraActionBarFrame.ignoreFramePositionManager = true
-	ExtraAbilityContainer.ignoreFramePositionManager = true
-
-	-- onshow/hide call UpdateManagedFramePositions on the blizzard end so
-	-- turn that bit off
-	ExtraAbilityContainer:SetScript("OnShow", nil)
-	ExtraAbilityContainer:SetScript("OnHide", nil)
-end
-
-function ExtraBarController:OnEnable()
-	self:ApplyTitanPanelWorkarounds()
+	_G['ExtraActionBarFrame'].ignoreFramePositionManager = true
 end
 
 function ExtraBarController:Load()
-    self.frame = ExtraBar:New()
+	self.frame = ExtraBar:New()
 end
 
 function ExtraBarController:Unload()
 	self.frame:Free()
-end
-
--- Titan panel will attempt to take control of the ExtraActionBarFrame and break
--- its position and ability to be usable. This is because Titan Panel doesn't
--- check to see if another addon has taken control of the bar
---
--- To resolve this, we call TitanMovable_AddonAdjust() for the extra ability bar
--- frames to let titan panel know we are handling positions for the extra bar
-function ExtraBarController:ApplyTitanPanelWorkarounds()
-    local adjust = _G.TitanMovable_AddonAdjust
-    if not adjust then return end
-
-    adjust('ExtraAbilityContainer', true)
-    adjust("ExtraActionBarFrame", true)
-    return true
 end
