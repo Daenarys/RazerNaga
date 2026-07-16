d189b5605a8d8cc04b619d1a589e39f5e762d325
Daenarys <androidwoz@gmail.com>
2026-07-16 08:05:37 +0200

    update tocs+libs

--- RazerNaga_XP/localization/localization.cn.lua
+++
@@ -1,9 +0,0 @@
-﻿
-local L = LibStub('AceLocale-3.0'):NewLocale('RazerNaga-XP', 'zhCN')
-if not L then return end
-
-L.Texture = '纹理'
-L.Width = '宽度'
-L.Height = '高度'
-L.AlwaysShowText = '始终显示文字'
-L.AlwaysShowXP = '始终显示经验'
