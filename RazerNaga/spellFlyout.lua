2b3e0731f1f269d707791864d7631cf62d44b71b
Daenarys <androidwoz@gmail.com>
2026-08-13 11:54:41 +0200

    skin updates

--- RazerNaga/spellFlyout.lua
+++ RazerNaga/spellFlyout.lua
@@ -429,7 +429,6 @@ function SpellFlyoutFrameMixin:CreateFlyoutButton(id)
 	local button = CreateFrame('CheckButton', name, self, 'SmallActionButtonTemplate, SecureActionButtonTemplate')
 
 	button:SetSize(28, 28)
-	button.cooldown:SetDrawBling(true)
 	_G[button:GetName().."Icon"]:SetTexCoord(4/64, 60/64, 4/64, 60/64)
 	button.NormalTexture:SetAlpha(0)
 	button.CheckedTexture:SetTexture([[Interface\Buttons\CheckButtonHilight]])
@@ -446,8 +445,6 @@ function SpellFlyoutFrameMixin:CreateFlyoutButton(id)
 	button.PushedTexture:ClearAllPoints()
 	button.PushedTexture:SetPoint("TOPLEFT")
 	button.PushedTexture:SetPoint("BOTTOMRIGHT")
-	button.cooldown:ClearAllPoints()
-	button.cooldown:SetAllPoints()
 
 	Mixin(button, SpellFlyoutButtonMixin)
 
